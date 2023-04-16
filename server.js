8ef393172cff8239ac67623267e7257ec912e9ed
karanveersingh <karanveer.singh@indifi.com>
2023-04-16 16:29:16 -0700

    updated urls

--- server.js
+++ server.js
@@ -36,10 +36,13 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header(
-    "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers: Content-Type, Authorization"
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
   );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   next();
 });
 
